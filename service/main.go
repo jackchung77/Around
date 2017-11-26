d2d4eb9278bf209e26c9c51486880faedb3184b3
Yu ChiehChung <JackChung@Yus-MacBook-Pro.local>
2017-11-26 14:06:48 -0800

    change external IP

--- service/main.go
+++ service/main.go
@@ -36,7 +36,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.196.114.189:9200"
+	ES_URL = "http://35.196.15.248:9200"
 )
 
 func containsFilteredWords(s *string) bool {
